04b914c4c4e165a5f4f98ade9b0f92bbb485a368
zhaofuguo <zhaofuguo@apipost.cn>
2024-12-13 13:54:04 +0800

    feat: 完善配置

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-import 'normalize.css';
-import '@/assets/css/reset.css';
+import 'normalize.css'; // 重置浏览器样式
+import '@/assets/css/reset.css'; // 重置样式
+import '@/assets/css/index.css'; // 全局样式
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
